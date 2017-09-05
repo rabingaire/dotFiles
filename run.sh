739ca40111798b6cedf415aacad70da048d53e47
Rabin Gaire <rabingaire20@gmail.com>
2017-09-05 22:14:03 +0545

    tmux status line theme added

--- run.sh
+++ run.sh
@@ -7,15 +7,10 @@ for software in $softwares; do
   fi
 done
 
-
-####### Variables
-
+####### Loop creating symlink 
 dir=~/dotFiles
 files="vimrc zshrc tmux.conf"
-vundle_link="https://github.com/VundleVim/Vundle.vim.git"
-vundle_path=~/.vim/bundle/Vundle.vim
 
-####### Loop creating symlink 
 for file in $files; do
   if [ ! -f ~/.$file ]; then
     echo "Moving any existing dotfiles from ~ to $dir"
@@ -25,12 +20,15 @@ for file in $files; do
   fi
 done
 
+####### Cloning vim packege manager vundle
+vundle_link="https://github.com/VundleVim/Vundle.vim.git"
+vundle_path=~/.vim/bundle/Vundle.vim
+
 if [ ! -d $vundle_path ];
   then git clone $vundle_link $vundle_path
 fi
 
-######## Adding theme to vim
-
+####### Adding theme to vim
 theme_name=Tomorrow-Night-Eighties.vim
 
 if [ ! -d ~/.vim/colors/ ]; then
@@ -39,4 +37,12 @@ fi
 
 if [ ! -f ~/.vim/colors/$theme_name ]; then
   ln -s $dir/vim/colors/$theme_name ~/.vim/colors/$theme_name
+fi
+
+####### Cloning tmux packege manager Tmux Plugin
+tmux_plugin_link="https://github.com/tmux-plugins/tpm"
+tmux_path=~/.tmux/plugins/tpm
+
+if [ ! -d $tmux_path ];
+  then git clone $tmux_plugin_link $tmux_path
 fi
